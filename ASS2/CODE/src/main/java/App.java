9423103f1fabee6fb49f611f24ef9c55687999db
Jack Davis <jackdavisworkperth@gmail.com>
2022-06-02 02:17:56 +0800

    added stuff

---
+++ ASS2/CODE/src/main/java/App.java
@@ -0,0 +1,12 @@
+package edu.curtin.app;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!!!!");
+        
+        // If you wish to change the name and/or package of the class containing 'main()', you 
+        // will also need to update the 'mainClass = ...' line in build.gradle.
+    }
+}
